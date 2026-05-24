48f195403a6caae5d37d04df6eafb5d2665de64e
Haseeb <ah085904@gmail.com>
2026-05-24 15:00:37 +0500

    form and dialog styles

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const dialogOpenBtn = document.querySelector("#dialog-open-btn");
 const dialog = document.querySelector("dialog");
+
+dialogOpenBtn.addEventListener("click", () => {
+	dialog.showModal();
+});
