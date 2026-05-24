6403fe26fbd7cee5ccd035911d902d4e2aaa4253
Haseeb <ah085904@gmail.com>
2026-05-24 15:11:40 +0500

    reference variables

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const dialogOpenBtn = document.querySelector("#dialog-open-btn");
 const dialog = document.querySelector("dialog");
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
+const pagesInput = document.querySelector("#number-input");
+const saveBtn = document.querySelector("#save-btn");
+const cancelBtn = document.querySelector("#cancel-btn");
+const statusBtn = document.querySelector("#status-btn");
+const removeBtn = document.querySelector("#remove-btn");
 
 dialogOpenBtn.addEventListener("click", () => {
 	dialog.showModal();
