65f28d100264155563303f7f019dec04e2819333
Haseeb <ah085904@gmail.com>
2026-05-24 16:11:38 +0500

    book constructor and add to library function

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const dialogOpenBtn = document.querySelector("#dialog-open-btn");
 const dialog = document.querySelector("dialog");
 const titleInput = document.querySelector("#title-input");
 const authorInput = document.querySelector("#author-input");
-const pagesInput = document.querySelector("#number-input");
+const pagesInput = document.querySelector("#pages-input");
 const saveBtn = document.querySelector("#save-btn");
 const cancelBtn = document.querySelector("#cancel-btn");
 const statusBtn = document.querySelector("#status-btn");
 const removeBtn = document.querySelector("#remove-btn");
+const checkInput = document.querySelector("#check-input");
 
 dialogOpenBtn.addEventListener("click", () => {
 	dialog.showModal();
@@ -14,6 +15,38 @@ dialogOpenBtn.addEventListener("click", () => {
 
 let library = [];
 
-function Book() {}
+function Book(title, author, pages, status) {
+	this.id = crypto.randomUUID();
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.status = status;
+}
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+	let title = titleInput.value;
+	let author = authorInput.value;
+	let pages = pagesInput.value;
+	let status = checkInput.checked ? "read" : "not read";
+	let flag = false;
+	for (let i = 0; i < library.length; i++) {
+		if (title === library[i].title) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+	}
+	if (flag === true) {
+		alert("this book already exists");
+	} else {
+		let newBook = new Book(title, author, pages, status);
+		library.push(newBook);
+	}
+}
+
+saveBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	addBookToLibrary();
+});
+
+function displayBooks() {}
